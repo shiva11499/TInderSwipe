c6a49669656653e807c0a6d476715d300bace3be
shiva11499 <gshivansh37@gmail.com>
2020-06-12 18:45:37 +0530

    WallpaperSwipe app

--- app/src/main/java/com/gshivansh37/wallpaperswipe/CardAdapter.java
+++ app/src/main/java/com/gshivansh37/wallpaperswipe/CardAdapter.java
@@ -3,11 +3,23 @@ package com.gshivansh37.wallpaperswipe;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.gshivansh37.wallpaperswipe.models.User;
+
+import java.util.List;
+
 public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardView> {
+    private List<User> users;
+
+    public CardAdapter(List<User> users) {
+        this.users = users;
+    }
 
     @NonNull
     @Override
@@ -18,17 +30,29 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardView> {
 
     @Override
     public void onBindViewHolder(@NonNull CardView holder, int position) {
-
+        holder.userNameTextView.setText(users.get(position).getName());
+        holder.userInfoTextView.setText(users.get(position).getInfo());
+        Glide.with(holder.imageView)
+                .load(users.get(position).getImageUrl())
+                .placeholder(R.drawable.background_gradient)
+                .into(holder.imageView);
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return users == null ? 0 : users.size();
     }
 
     static class CardView extends RecyclerView.ViewHolder {
+        private final ImageView imageView;
+        private final TextView userNameTextView;
+        private final TextView userInfoTextView;
+
         public CardView(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.user_image);
+            userNameTextView = itemView.findViewById(R.id.user_name);
+            userInfoTextView = itemView.findViewById(R.id.user_info);
         }
     }
 }
--- app/src/main/java/com/gshivansh37/wallpaperswipe/MainActivity.java
+++ app/src/main/java/com/gshivansh37/wallpaperswipe/MainActivity.java
@@ -3,8 +3,19 @@ package com.gshivansh37.wallpaperswipe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
+import com.gshivansh37.wallpaperswipe.models.User;
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
 import com.yuyakaido.android.cardstackview.CardStackView;
+import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.Duration;
+import com.yuyakaido.android.cardstackview.RewindAnimationSetting;
+import com.yuyakaido.android.cardstackview.StackFrom;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +24,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CardStackView stackView = findViewById(R.id.card_stack_view);
-        stackView.setAdapter(new CardAdapter());
+        final CardStackView stackView = findViewById(R.id.card_stack_view);
+
+        // Configuring Layout Manager
+        CardStackLayoutManager layoutManager = new CardStackLayoutManager(this);
+        layoutManager.setStackFrom(StackFrom.Top);
+        layoutManager.setVisibleCount(3);
+        RewindAnimationSetting setting = new RewindAnimationSetting.Builder()
+                .setDirection(Direction.Left)
+                .setDuration(Duration.Slow.duration)
+                .setInterpolator(new AccelerateDecelerateInterpolator())
+                .build();
+        layoutManager.setRewindAnimationSetting(setting);
+
+
+        stackView.setLayoutManager(layoutManager);
+        stackView.setAdapter(new CardAdapter(initData()));
+
+        findViewById(R.id.action_bolt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stackView.rewind();
+            }
+        });
+    }
+
+    private List<User> initData() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Renae Will", "Surgeon", "https://media.idownloadblog.com/wp-content/uploads/2017/01/vellum-wallpaper-1.jpg"));
+        users.add(new User("Clark Gilbert", "Song writer", "https://media.idownloadblog.com/wp-content/uploads/2017/01/vellum-wallpaper-4.jpg"));
+        users.add(new User("Paulina Eva", "Councillor", "https://uploads.disquscdn.com/images/ba43d6a0587e027239ed64723c7cb7920e5b63e76e508edc10432434761a0db9.jpg"));
+        users.add(new User("Malcolm Jinny", "Costume designer", "https://media.idownloadblog.com/wp-content/uploads/2017/01/vellum-wallpaper-3.jpg"));
+        users.add(new User("Racquel Tamera", "Dietician", "https://media.idownloadblog.com/wp-content/uploads/2017/01/vellum-wallpaper-2.jpg"));
+        return users;
     }
 }
